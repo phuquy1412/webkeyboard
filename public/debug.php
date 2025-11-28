015592de6af67ccd2c88898177a3840bb50746c7
quyh2 <hophuquy.work@gmail.com>
2025-11-28 09:28:18 +0700

    push lan 9

---
+++ public/debug.php
@@ -0,0 +1,7 @@
+<?php
+// debug.php - Temporary debug helper. REMOVE THIS FILE after use.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+echo "DEBUG MODE - remove this file when finished\n";
+phpinfo();
